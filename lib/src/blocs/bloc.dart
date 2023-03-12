67edc25b43975462db3d26089c222dcd20c7b866
Seun <oniyajoeolaiya@gmail.com>
2023-03-12 21:39:55 +0100

    last commit on login_bloc

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -1,9 +1,13 @@
 import 'dart:async';
 import 'validators.dart';
+import 'package:rxdart/rxdart.dart';
 
 class Bloc with Validators {
-  final _email = StreamController<String>();
-  final _password = StreamController<String>();
+  final _email = BehaviorSubject();
+  final _password = BehaviorSubject();
+
+  Stream<bool> get submitValid =>
+      Rx.combineLatest2(email, password, (e, p) => true);
 
   // Add data to stream
   Stream<String> get email => _email.stream.transform(validateEmail);
@@ -12,11 +16,20 @@ class Bloc with Validators {
 
   // Change data
   Function(String) get changeEmail => _email.sink.add;
+
   Function(String) get changePassword => _password.sink.add;
+
+  submit() {
+    final validEmail = _email.value;
+    final validPassword = _password.value;
+
+    print('this is the valid email $validEmail');
+    print('this is the valid password $validPassword');
+
+  }
 
   dispose() {
     _email.close();
     _password.close();
   }
 }
-
--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -2,13 +2,11 @@ import 'package:flutter/material.dart';
 import '../blocs/bloc.dart';
 import '../blocs/provider.dart';
 
-
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
     final bloc = Provider.of(context);
 
     return Container(
@@ -17,8 +15,10 @@ class LoginScreen extends StatelessWidget {
         children: [
           emailField(bloc),
           passwordField(bloc),
-          const SizedBox(height: 25.0,),
-          submitButton(),
+          const SizedBox(
+            height: 25.0,
+          ),
+          submitButton(bloc),
         ],
       ),
     );
@@ -26,50 +26,52 @@ class LoginScreen extends StatelessWidget {
 
   Widget emailField(Bloc bloc) {
     return StreamBuilder<String>(
-      stream: bloc.email,
-      builder: (context, AsyncSnapshot<String> snapshot) {
-        return TextField(
-          onChanged: (value){
-            bloc.changeEmail(value);
-          },
-          keyboardType: TextInputType.emailAddress,
-          decoration: InputDecoration(
-            hintText: 'you@example.com',
-            labelText: 'EMail Address',
-            errorText: '${snapshot.error}',
-          ),
-        );
-      }
-    );
+        stream: bloc.email,
+        builder: (context, AsyncSnapshot<String> snapshot) {
+          return TextField(
+            onChanged: (value) {
+              bloc.changeEmail(value);
+            },
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+              hintText: 'you@example.com',
+              labelText: 'EMail Address',
+              errorText: snapshot.hasError ? '${snapshot.error}' : '',
+            ),
+          );
+        });
   }
 
   Widget passwordField(Bloc bloc) {
     return StreamBuilder<Object>(
-      stream: bloc.password,
-      builder: (context, snapshot) {
-        return TextField(
-          onChanged: bloc.changePassword,
-          decoration: InputDecoration(
-            hintText: 'Password',
-            labelText: 'Password',
-            errorText: '${snapshot.error}',
-          ),
-        );
-      }
-    );
+        stream: bloc.password,
+        builder: (context, snapshot) {
+          return TextField(
+            onChanged: bloc.changePassword,
+            decoration: InputDecoration(
+              hintText: 'Password',
+              labelText: 'Password',
+              errorText: snapshot.hasError ? '${snapshot.error}' : '',
+            ),
+          );
+        });
   }
 
-  Widget submitButton() {
-    return ElevatedButton(
-      onPressed: () {},
-      style: const ButtonStyle(
-        backgroundColor: MaterialStatePropertyAll(
-          Colors.blue,
-        ),
-      ),
-      child: const Text(
-        'Login',
-      ),
-    );
+  Widget submitButton(Bloc bloc) {
+    return StreamBuilder(
+        stream: bloc.submitValid,
+        builder: (context, snapshot) {
+          return ElevatedButton(
+            onPressed: snapshot.hasData ? bloc.submit : null,
+            style: const ButtonStyle(
+              backgroundColor: MaterialStatePropertyAll(
+                Colors.blue,
+              ),
+            ),
+            child: const Text(
+              'Login',
+            ),
+          );
+        });
   }
 }
